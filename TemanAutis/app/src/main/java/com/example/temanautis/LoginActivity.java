e33312c47bfdfa03fb91a60bf3d191e9f4ff7eb8
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2021-07-11 15:16:06 +0700

    LoginRetrofit

--- TemanAutis/app/src/main/java/com/example/temanautis/LoginActivity.java
+++ TemanAutis/app/src/main/java/com/example/temanautis/LoginActivity.java
@@ -5,15 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.temanautis.API.APIRequestData;
+import com.example.temanautis.API.RetroServer;
+import com.example.temanautis.Model.Login;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    String Email, Password;
+    APIRequestData apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.editTextEmail);
+        etPassword = findViewById(R.id.editTextPassword);
 
         if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
@@ -21,14 +38,42 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+
+
     public void onLoginClick(View View){
         startActivity(new Intent(this,RegisterActivity.class));
         overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
 
     }
     public void onMasukClick(View view){
-        startActivity(new Intent(LoginActivity.this, TabActivity.class));
-        finish();
+        apiInterface = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Log.d("TAG", "onMasukClick: "+etEmail.getText().toString());
+        Call<Login> loginCall = apiInterface.loginresponse(etEmail.getText().toString(),etPassword.getText().toString());
+        loginCall.enqueue(new Callback<Login>() {
+            @Override
+            public void onResponse(Call<Login> call, Response<Login> response) {
+                if(response.isSuccessful()){
+                    startActivity(new Intent(LoginActivity.this, TabActivity.class));
+                    finish();
+                }else {
+                    Toast.makeText(LoginActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
+                    Log.d("onResponse", "onResponse: " + response.message());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<Login> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, "Gagal"+t.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d("onFailure", "onFailure: " + t.getMessage());
+            }
+        });
     }
+
+    public void login(String email, String password){
+
+    }
+
+
 
 }
---
+++ TemanAutis/app/src/main/java/com/example/temanautis/Model/LoginData.java
@@ -0,0 +1,83 @@
+package com.example.temanautis.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LoginData {
+
+	@SerializedName("updated_at")
+	private String updatedAt;
+
+	@SerializedName("id_hak_akses")
+	private int idHakAkses;
+
+	@SerializedName("name")
+	private String name;
+
+	@SerializedName("created_at")
+	private String createdAt;
+
+	@SerializedName("email_verified_at")
+	private Object emailVerifiedAt;
+
+	@SerializedName("id")
+	private int id;
+
+	@SerializedName("email")
+	private String email;
+
+	public void setUpdatedAt(String updatedAt){
+		this.updatedAt = updatedAt;
+	}
+
+	public String getUpdatedAt(){
+		return updatedAt;
+	}
+
+	public void setIdHakAkses(int idHakAkses){
+		this.idHakAkses = idHakAkses;
+	}
+
+	public int getIdHakAkses(){
+		return idHakAkses;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setCreatedAt(String createdAt){
+		this.createdAt = createdAt;
+	}
+
+	public String getCreatedAt(){
+		return createdAt;
+	}
+
+	public void setEmailVerifiedAt(Object emailVerifiedAt){
+		this.emailVerifiedAt = emailVerifiedAt;
+	}
+
+	public Object getEmailVerifiedAt(){
+		return emailVerifiedAt;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+}
