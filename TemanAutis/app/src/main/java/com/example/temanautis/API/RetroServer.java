f9e7d4b7e5fec0af1c493b36081a16d76cbc56e0
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2021-07-11 19:02:45 +0700

    update menambah ip network

--- TemanAutis/app/src/main/java/com/example/temanautis/API/RetroServer.java
+++ TemanAutis/app/src/main/java/com/example/temanautis/API/RetroServer.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    private static final String baseurl = "http://192.168.43.141:8000/api/";
+    private static final String baseurl = "http://192.168.1.9:8000/api/";
     private static Retrofit retro;
 
     public static Retrofit konekRetrofit(){
