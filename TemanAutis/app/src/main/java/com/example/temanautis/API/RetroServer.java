d36b562f1f6256c30904c0b50dd339fcfa6cc20c
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2021-07-12 22:18:49 +0700

    Up

--- TemanAutis/app/src/main/java/com/example/temanautis/API/RetroServer.java
+++ TemanAutis/app/src/main/java/com/example/temanautis/API/RetroServer.java
@@ -4,11 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-<<<<<<< Updated upstream
-    private static final String baseurl = "http://192.168.45.184:8000/api/";
-=======
-    private static final String baseurl = "http://10.212.17.220:8000/api/";
->>>>>>> Stashed changes
+    private static final String baseurl = "http://192.168.1.9:8000/api/";
     private static Retrofit retro;
 
     public static Retrofit konekRetrofit(){
