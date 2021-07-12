9aa00092be749a87d913f1ccff50e189b2469f59
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2021-07-12 16:39:04 +0700

    Session Login

--- TemanAutis/app/src/main/java/com/example/temanautis/Fragment3.java
+++ TemanAutis/app/src/main/java/com/example/temanautis/Fragment3.java
@@ -1,5 +1,6 @@
 package com.example.temanautis;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.temanautis.Model.SessionManager;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +24,10 @@ public class Fragment3 extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    TextView etName;
+    String name;
+    SessionManager sessionManager;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -59,6 +68,13 @@ public class Fragment3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_3, container, false);
+        View view = inflater.inflate(R.layout.fragment_3, container, false);
+        sessionManager = new SessionManager(getContext());
+        etName = view.findViewById(R.id.etMainName);
+        name = (String) sessionManager.getUserDetail().get(SessionManager.NAME);
+        etName.setText(name);
+        return view;
+
     }
+
 }
