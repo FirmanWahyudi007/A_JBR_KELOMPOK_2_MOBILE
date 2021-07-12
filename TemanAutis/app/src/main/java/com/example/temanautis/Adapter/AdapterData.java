e1f68aeccc5002c2f180fed895c3f490a9fd7517
wahyudianpratiwi1 <71805820+wahyudianpratiwi1@users.noreply.github.com>
2021-07-12 21:46:04 +0700

    layout

--- TemanAutis/app/src/main/java/com/example/temanautis/Adapter/AdapterData.java
+++ TemanAutis/app/src/main/java/com/example/temanautis/Adapter/AdapterData.java
@@ -44,8 +44,14 @@ public class AdapterData extends RecyclerView.Adapter<AdapterData.HolderData>{
             holder.tvIsi.setText(Html.fromHtml(dm.getIsi_artikel(), Html.FROM_HTML_MODE_LEGACY));
         } else
             holder.tvIsi.setText(Html.fromHtml(dm.getIsi_artikel()));
+<<<<<<< Updated upstream
         String url = "http://192.168.1.9:8000/images/"+dm.getSampul();
         Picasso.get()
+=======
+        String url = "http://192.168.43.141:8000/images/"+dm.getSampul();
+        Picasso
+                .get()
+>>>>>>> Stashed changes
                 .load(url)
                 .placeholder(R.drawable.ic_launcher_background)
                 .error(R.drawable.ic_launcher_background)
