d2389916b1a1501e633dcaeb8b002f1f13adae42
wahyudianpratiwi1 <71805820+wahyudianpratiwi1@users.noreply.github.com>
2021-07-15 23:29:27 +0700

    splash

---
+++ TemanAutis/app/src/main/java/com/example/temanautis/Splash.java
@@ -0,0 +1,34 @@
+package com.example.temanautis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import java.util.logging.LogRecord;
+
+public class Splash extends AppCompatActivity {
+    Animation zoom;
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        zoom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
+        img = findViewById(R.id.image);
+        img.startAnimation(zoom);
+
+        android.os.Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),Slider.class);
+                startActivity(i);
+                finish();
+            }
+        },4000);
+    }
+}
